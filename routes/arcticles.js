9270f5c7f5fe37034df4d53515abc5a09a7ea379
shreya0181 <shreya171769.cse@chitkara.edu.in>
2020-11-07 12:38:27 +0530

    adding action file _form_feild

--- routes/arcticles.js
+++ routes/arcticles.js
@@ -2,4 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
+router.get('/new',(req, res)=>{
+    res.render('articles/new');
+
+})
+
+
+router.post('/', (req, res)=>{
+    
+})
 module.exports = router;
